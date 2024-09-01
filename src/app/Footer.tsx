856e6601822de0f342f094d2939fd55fa0071596
ShaQay3Q <shaghay3gh@gmail.com>
2024-09-01 16:55:20 +0200

    initiating portfolio with next.js

---
+++ src/app/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return (
+        <div>
+            <p className="mb-3 text-2xl font-semibold bg-red-600">
+                Header
+            </p>
+        </div>
+    )
+}
---
+++ src/app/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return (
+        <div>
+            <p className="mb-3 text-2xl font-semibold bg-green-600">
+                Header
+            </p>
+        </div>
+    )
+}
